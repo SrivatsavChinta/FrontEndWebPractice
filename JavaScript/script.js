e5def71ee24a581ce5053805f9a24e90e3a954dc
srivatsavchinta2002@gmail.com <srivatsavchinta2002@gmail.com>
2024-03-27 17:43:41 +0530

    completed fundamentals in JS udemy

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// let hasDriversLicense = false
+// const passTest = true
+// if(passTest) hasDriversLicense = true
+// if(hasDriversLicense) console.log('I can drive!')
+// future reserved words
+// const interface = 'Audio'
+
+//function declaration and expression
+const func = (n) => {
+    console.log(n)
+}
+let temp = func(4)
